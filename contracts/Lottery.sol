e162ad86cba2c7791fa2c25ab3a48c1ccb2072a6
Darwin Benzi <92586709+DarwinBenzi@users.noreply.github.com>
2022-06-24 15:40:25 +0300

    Creating contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,5 +1,36 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.9.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract Lottery {}
+contract Lottery {
+    address payable[] public participants;
+    mapping(index => address) public participantWithPosition;
+    uint256 noOfParticipants = 0;
+    AggregatorV3Interface priceFeed;
+    address Admin;
+    uint256 entryFeeInCash;
+    uint256 priceFeedEth;
+
+    constructor(address _priceFeed) public {
+        entryFeeInCash = 50;
+        Admin = msg.sender;
+        priceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    function addParticipant() public payable {
+        require(msg.value < getEntranceFee, "Not enough eth!");
+        participants.push(msg.sender);
+        participantWithPosition[noOfParticipants++] = msg.sender;
+    }
+
+    function getEntranceFee(uint256 _value) public returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        priceFeedEth = answer * (10**10);
+        return ((priceFeedEth * (entryFeeInCash * (10**15))) / 10**18);
+    }
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
