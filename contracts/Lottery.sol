5df60856929f3f08ffaa8524cafde46ed2e4be46
Darwin Benzi <92586709+DarwinBenzi@users.noreply.github.com>
2022-06-26 13:18:42 +0300

    Added license

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Lottery {
     address payable[] public participants;
