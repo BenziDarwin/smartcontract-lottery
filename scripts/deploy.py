dfd31d855372ab0b29b86ecaaf0c1be56438a1e9
Darwin Benzi <92586709+DarwinBenzi@users.noreply.github.com>
2022-06-28 13:52:19 +0300

    Adding to the main deploy contract

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 from brownie import Lottery, network, MockV3Aggregator, config
-from scripts.helpful_scripts import deploy_contract
+from scripts.helpful_scripts import deploy_contract, choose_account
 
 
 def deploy():
@@ -8,5 +8,13 @@ def deploy():
     print("Contract successfully deployed!")
 
 
+def start_lottery():
+    account = choose_account()
+    lottery = deploy_contract()
+    lottery.startLottery({"from": account})
+    print("The lottery has begun!")
+
+
 def main():
     deploy()
+    start_lottery()
