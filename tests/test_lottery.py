13b353bcf6556897e80a8484a76be069c05213e7
Darwin Benzi <92586709+DarwinBenzi@users.noreply.github.com>
2022-06-30 21:10:45 +0300

    Final commit

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,27 +1,22 @@
+from brownie import Lottery
 from scripts.helpful_scripts import choose_account, deploy_contract
 
 
 def test_deploy():
-    account = choose_account()
     lottery = deploy_contract()
-    assert lottery.showAdminAccount() == account
+    assert Lottery[0] == lottery
 
 
-def test_request_random_words():
+def test_can_add_participant():
     account = choose_account()
     lottery = deploy_contract()
-    txn = lottery.requestRandomWords({"from": account})
-    txn.wait(2)
-    assert lottery.showRandomWords() != 0
+    txn = lottery.startLottery({"from": account})
+    txn.wait(0)
+    value = lottery.showEntranceFee({"from": account})
+    lottery.addParticipant({"from": account, "value": value})
+    assert lottery.participants(0) == account
 
 
 def test_get_entrance_fee():
     lottery = deploy_contract()
-    lottery.setEntranceFee()
-    entranceFee = lottery.showEntranceFee()
-    assert entranceFee != 0
-
-
-def test_show_recent_winner():
-    lottery = deploy_contract()
-    assert lottery.recentWinner() != "0x0000000000000000000000000000000000000000"
+    lottery.showEntranceFee() != 0
